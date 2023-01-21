b08123e7fcb41e611f40e52d55e0323e5bd743ea
Mateoferrer66 <mateoferrer66@gmail.com>
2023-01-20 23:54:11 -0500

    es10

---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,13 @@
+const hello = "       Hello      World!   ";
+console.log(hello.trimStart());
+
+console.log(hello.trimEnd());
+
+console.log(hello.trim());
+
+
+// El método trimStart o trimLeft elimina los espacios al inicio.
+
+// El método trimEnd o trimRight elimina los espacios al final.
+
+// El método trim elimina los espacios en blanco al inicio y al final.
